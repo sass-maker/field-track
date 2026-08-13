185e2d8cea20351b1fc332a8d2940013fc57d6b3
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2026-08-14 00:38:56 +0530

    fix: use React Native 0.87 application entry point

--- apps/employee-mobile/android/app/src/main/java/com/fieldtrack/employee/MainApplication.kt
+++ apps/employee-mobile/android/app/src/main/java/com/fieldtrack/employee/MainApplication.kt
@@ -4,8 +4,8 @@ import android.app.Application
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
+import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.loadReactNative
 import com.fieldtrack.employee.tracking.FieldTrackingPackage
 import com.fieldtrack.employee.tracking.TrackingRuntime
 
